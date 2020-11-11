6510a28194742e697a75188b1bd00975551022e8
juandeto <juandeto10@gmail.com>
2020-11-10 21:33:56 -0300

    push

--- nookweb/src/containers/Carrito/ContactData/ContactData.js
+++ nookweb/src/containers/Carrito/ContactData/ContactData.js
@@ -156,7 +156,7 @@ class ContactData extends Component {
     };
     axios.post('https://nookserver.herokuapp.com/mercadopago/create', {
       headers: {
-        Authorization: 'Access-Control-Allow-Headers", "X-Requested-With, content-type',
+        Authorization: 'Access-Control-Allow-Headers, X-Requested-With, content-type',
       },
       data: postData,
       order: order,
